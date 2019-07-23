71adf937986d5178d5e4324c8e9cb0d14ada1df7
MrFlexi <szaroletta@hotmail.com>
2019-07-23 18:52:11 +0200

    New error protokoll

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ArduinoJson.h>
 #include "esp_system.h"
 #include "webserver.h"
+#include "tasks.h"
 
 //-----------------------------------------------------------------------
 // RTOS
@@ -20,8 +21,6 @@ const float alive_msg_intervall = 20; // 60 seconds
 int roundtrips = 0;
 int queueSize = 10;
 
-
-
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
@@ -30,10 +29,10 @@ extern "C"
   uint8_t temprature_sens_read();
 }
 
-
-
 message_buffer_t gs_message_buffer;
 message_buffer_t gs_message_queue_out;
+
+error_message_t gs_error_message;
 
 void t_cpu_temp(void *parameter)
 {
@@ -51,7 +50,6 @@ void t_cpu_temp(void *parameter)
   }
 }
 
-
 void t_alive_msg(void *parameter)
 {
   // Task bound to core 0, Prio 0 =  very low
@@ -67,10 +65,6 @@ void t_alive_msg(void *parameter)
     Serial.println(xPortGetCoreID());
 
     gs_message_buffer.roundtrips = roundtrips;
-
-    serializeJson(doc, JsonStr);
-    ws.textAll(JsonStr);
-    serializeJsonPretty(doc, Serial);
     delay(20000);
   }
 }
@@ -78,12 +72,44 @@ void t_alive_msg(void *parameter)
 void t_broadcast_message(void *parameter)
 {
   // Task bound to core 0, Prio 0 =  very low
+
+  error_message_t error_message;
+
   String JsonStr;
 
   for (;;)
   {
-    JsonStr = message_buffer_to_jsonstr(gs_message_buffer);
-    ws.textAll(JsonStr);
+    if (queue != NULL)
+    {
+
+      int messagesWaiting = uxQueueMessagesWaiting(queue);
+      Serial.print("Messages waiting: ");
+      Serial.println(messagesWaiting);
+
+     gs_message_buffer.error_msg_count = messagesWaiting; 
+
+      if (messagesWaiting > 0)
+      {
+        
+        for (int i = 0; i < messagesWaiting; i++)
+        {
+
+          xQueueReceive(queue, &error_message, portMAX_DELAY);
+          Serial.print(error_message.priority);
+          Serial.print("|");
+          Serial.println(error_message.title);
+
+          // Put into array
+          error_tab[i].priority = error_message.priority;
+          error_tab[i].title     = error_message.title;
+
+        }
+      }
+        
+      JsonStr = message_buffer_to_jsonstr(gs_message_buffer, error_tab);
+      ws.textAll(JsonStr);
+    }
+
     delay(10000);
   }
 }
@@ -116,138 +142,26 @@ void create_Tasks()
       &task_cpu_temp); /* Task handle. */
 }
 
-void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
-{
-  if (type == WS_EVT_CONNECT)
-  {
-    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-    client->ping();
-  }
-  else if (type == WS_EVT_DISCONNECT)
-  {
-    Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
-  }
-  else if (type == WS_EVT_ERROR)
-  {
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
-  }
-  else if (type == WS_EVT_PONG)
-  {
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
-  }
-  else if (type == WS_EVT_DATA)
-  {
-    AwsFrameInfo *info = (AwsFrameInfo *)arg;
-    String msg = "";
-    if (info->final && info->index == 0 && info->len == len)
-    {
-      //the whole message is in a single frame and we got all of it's data
-      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
-
-      if (info->opcode == WS_TEXT)
-      {
-        for (size_t i = 0; i < info->len; i++)
-        {
-          msg += (char)data[i];
-        }
-      }
-      else
-      {
-        char buff[3];
-        for (size_t i = 0; i < info->len; i++)
-        {
-          sprintf(buff, "%02x ", (uint8_t)data[i]);
-          msg += buff;
-        }
-      }
-      Serial.printf("%s\n", msg.c_str());
-
-      if (info->opcode == WS_TEXT)
-        Serial.println("I got your text message");
-      else
-        Serial.println("I got your binary message");
-    }
-    else
-    {
-      //message is comprised of multiple frames or the frame is split into multiple packets
-      if (info->index == 0)
-      {
-        if (info->num == 0)
-          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
-        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
-      }
-
-      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
-
-      if (info->opcode == WS_TEXT)
-      {
-        for (size_t i = 0; i < len; i++)
-        {
-          msg += (char)data[i];
-        }
-      }
-      else
-      {
-        char buff[3];
-        for (size_t i = 0; i < len; i++)
-        {
-          sprintf(buff, "%02x ", (uint8_t)data[i]);
-          msg += buff;
-        }
-      }
-      Serial.printf("%s\n", msg.c_str());
-
-      if ((info->index + len) == info->len)
-      {
-        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-        if (info->final)
-        {
-          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
-          if (info->message_opcode == WS_TEXT)
-            client->text("I got your text message");
-          else
-            client->binary("I got your binary message");
-        }
-      }
-    }
-  }
-}
-
 void setup()
 {
   Serial.begin(115200);
 
   // RTOS Initialisation
-  queue = xQueueCreate(queueSize, sizeof(message_buffer_t));
+  queue = xQueueCreate(queueSize, sizeof(error_message_t));
   if (queue == NULL)
   {
     Serial.println("Error creating the queue");
   }
 
-  gs_message_buffer.roundtrips = 1;
-  gs_message_buffer.temperatur = 150;
-  xQueueSend(queue, &gs_message_buffer, portMAX_DELAY);
+  gs_error_message.priority = "High";
+  gs_error_message.title = "CPU temperatur high";
+  xQueueSend(queue, &gs_error_message, portMAX_DELAY);
 
-  gs_message_buffer.roundtrips = 2;
-  gs_message_buffer.temperatur = 200;
-  xQueueSend(queue, &gs_message_buffer, portMAX_DELAY);
+  gs_error_message.priority = "Low";
+  gs_error_message.title = "Lora Data received";
+  xQueueSend(queue, &gs_error_message, portMAX_DELAY);
 
-  if (queue != NULL)
-  {
-
-    int messagesWaiting = uxQueueMessagesWaiting(queue);
-    Serial.print("Messages waiting: "); Serial.println(messagesWaiting);
-
-    for (int i = 0; i < messagesWaiting; i++)
-    {
-
-      xQueueReceive(queue, &gs_message_queue_out, portMAX_DELAY);
-      Serial.print(gs_message_queue_out.roundtrips);
-      Serial.print("|");
-      Serial.println(gs_message_queue_out.temperatur);
-    }
-  }
-
+  
   create_Tasks();
 
   // External File System Initialisation
